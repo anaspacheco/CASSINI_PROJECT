89b446c9f760531cec799cf5eb68885a4a6edc26
Ana Pacheco <121977567+anaspacheco@users.noreply.github.com>
2023-07-25 09:27:20 -0400

    Add files via upload

--- IMAGES/image_extract.py
+++ IMAGES/image_extract.py
@@ -1,53 +1,54 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from rasterio.plot import show
 import datetime
 import rasterio
 import imageio
 import sys
 import pvl
+import requests
 
-image_id = "N1610585886_1"
+# URL of the image
+image_url = "https://planetarydata.jpl.nasa.gov/img/data/cassini/cassini_orbiter/coiss_2050/data/1607482567_1607567228/N1607484037_1.IMG"
 
-#https://planetarydata.jpl.nasa.gov/img/data/cassini/cassini_orbiter/coiss_2050/data/1607482567_1607567228/N1607484037_1.IMG
+#ImageID (will automate this later on)
+image_id = "N1607484037_1"
 
-# Now opening the PDS format (.IMG and .LBL )
-with rasterio.open('/Users/anasofiapacheco/Desktop/'+image_id+'.IMG', 'r') as src:
-    image_data = src.read(1)
-
+with rasterio.open(image_url) as dataset:
+    image_data = dataset.read(1)  # Single-band image
 
 # Plot the image using matplotlib
-plt.figure(figsize=(8, 8))
 plt.imshow(image_data, cmap='gray')
 plt.title(image_id)
 plt.xlabel('Pixel Column')
 plt.ylabel('Pixel Row')
 plt.show()
 
-#Add contour
+#Possibility: add contour
 '''
 contour_levels = np.linspace(image_data.min(), image_data.max(), 10) 
 plt.contour(image_data, levels=contour_levels, colors='red')
 plt.show()
 '''
 
-label_file_path ='/Users/anasofiapacheco/Desktop/'+image_id+'.LBL'
+#Label of image, to be outputed to a file
 
-# Loading the label file using pvl
-with open(label_file_path, 'r') as f:
-    label_data = pvl.load(f)
+label_url = "https://planetarydata.jpl.nasa.gov/img/data/cassini/cassini_orbiter/coiss_2050/data/1607482567_1607567228/N1607484037_1.LBL"
+
+label_response = requests.get(label_url)
+label_response.raise_for_status()  # Check if the request was successful
+label = label_response.text
+
+# Output the parsed PVL content to a new file
+output_file_path = image_id +"_metadata" + ".txt"  # Replace with the desired output file path
+with open(output_file_path, "w") as output_file:
+    output_file.write(label)
 
 #Next steps: would need to search for this image_id in the large database of all label files (for all volumes -- maybe using MySQL) to find the actual
 #metadata that we actually care about 
 
-# Printing the label information
-print("Label Information:")
-print("===================")
-for key, value in label_data.items():
-    print(f"{key}: {value}")
-
-#Need to automate this later on
-#Note: can find 16-bit greyscale TIFF files at 1024x1024 here: coiss_20XX/extras/tiff/XXXXXXXX_XXXXXXXXX/
+#Another possibility: can find 16-bit greyscale TIFF files at 1024x1024 here: coiss_20XX/extras/tiff/XXXXXXXX_XXXXXXXXX/
 
 '''
 image_path = '/Users/anasofiapacheco/Desktop/N1604811666_1.IMG.tiff'
